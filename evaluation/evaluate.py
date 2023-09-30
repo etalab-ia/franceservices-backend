5b33fea8806f96dc1ea44192727743463992c235
dtrckd <dtrckd@gmail.com>
2023-10-01 01:56:18 +0200

    feat: add support for reference model (prompt)

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -5,15 +5,22 @@ from pprint import pprint
 
 import numpy as np
 import pandas as pd
-import requests
+from elasticsearch import Elasticsearch
+from qdrant_client import QdrantClient
+from qdrant_client import models as QdrantModels
 
 from .extract import extract
+from .utils import generate, get_embedding_e5
+
+
+def embed(text: str) -> list:
+    return get_embedding_e5(text)
 
 
 class EVAL(object):
     ROUTES = {
         "miaou": {"url": "http://localhost:8081", "prompt_maker": "_make_prompt"},
-        "reference": {"url": "http://localhost:8081", "prompt_maker": "_make_prompt_2"},
+        "reference": {"url": "http://localhost:8001", "prompt_maker": "_make_prompt_2"},
     }
 
     def __init__(self, model, version, limit=None, yes=False, n_async=70):
@@ -37,25 +44,92 @@ class EVAL(object):
 
     @staticmethod
     def _make_prompt_2(exp: dict, mode="simple", **kwargs) -> str:
-        # TO COMPLETE
-        if mode == "simmle":
-            prompt = f"""Question soumise au service {exp["intitule_typologie_1"]}: {exp["description"]}
-        ---Réponse : """
+        institution = exp["intitule_typologie_1"]
+        institution = institution + " " if institution else ""
+        text = exp["description"]
+
+        prompt = []
+        if mode == "simple":
+            prompt.append("Mode simple")
+            prompt.append(f"Question soumise au service {institution}: {text}")
+            prompt.append("###Réponse : \n")
+            prompt = "\n\n".join(prompt)
+        elif mode == "experience":
+            prompt.append("Mode expérience")
+            prompt.append(f"Question soumise au service {institution} : {text}")
+
+            # Rag
+            retrieves = ["id_experience", "description"]
+            _extract = lambda x: dict((r, x[r]) for r in retrieves)
+            embedding = embed(text)
+            client = QdrantClient(url="http://localhost:6333", grpc_port=6334, prefer_grpc=True)
+            index_name = "experiences"
+            # Filter on institution
+            query_filter = None
+            if institution:
+                query_filter = QdrantModels.Filter(
+                    must=[
+                        QdrantModels.FieldCondition(
+                            key="intitule_typologie_1",
+                            match=QdrantModels.MatchValue(
+                                value=institution,
+                            ),
+                        )
+                    ]
+                )
+            res = client.search(
+                collection_name=index_name,
+                query_vector=embedding,
+                query_filter=query_filter,
+                limit=3,
+            )
+            es = Elasticsearch("http://localhost:9202", basic_auth=("elastic", "changeme"))
+            # @Debug : qdrant doesnt accept the hash id as string..
+            _uid = lambda x: bytes.fromhex(x.replace("-", "")).decode("utf8")
+            hits = [_extract(es.get(index=index_name, id=_uid(x.id))["_source"]) for x in res if x]
+            chunks = [f'{x["id_experience"]} : {x["description"]}' for x in hits]
+            chunks = "\n\n".join(chunks)
+            prompt.append(f"Expériences :\n\n {chunks}")
+
+            prompt.append("###Réponse : \n")
+            prompt = "\n\n".join(prompt)
+        elif mode == "expert":
+            prompt.append("Mode expert")
+            prompt.append(f"Expérience : {text}")
+            # Get reponse...
+            rep1 = generate(EVAL.ROUTES["miaou"], {"max_tokens": 500, "temperature": 0.2}, text)
+            rep1 = "".join(rep1)
+            prompt.append(f"Réponse :\n\n {rep1}")
+
+            # Rag
+            retrieves = ["title", "url", "text", "context"]
+            _extract = lambda x: dict((r, x[r]) for r in retrieves)
+            embedding = embed(text)
+            client = QdrantClient(url="http://localhost:6333", grpc_port=6334, prefer_grpc=True)
+            index_name = "chunks"
+            res = client.search(
+                collection_name=index_name, query_vector=embedding, query_filter=None, limit=3
+            )
+            es = Elasticsearch("http://localhost:9202", basic_auth=("elastic", "changeme"))
+            # @Debug : qdrant doesnt accept the hash id as string..
+            _uid = lambda x: bytes.fromhex(x.replace("-", "")).decode("utf8")
+            hits = [_extract(es.get(index=index_name, id=_uid(x.id))["_source"]) for x in res if x]
+            chunks = [
+                f'{x["url"]} : {x["title"] + (" > "+x["context"]) if x["context"] else ""}\n{x["text"]}'
+                for x in hits
+            ]
+            chunks = "\n\n".join(chunks)
+            prompt.append(f"Fiches :\n\n {chunks}")
+
+            prompt.append("###Réponse : \n")
+            prompt = "\n\n".join(prompt)
         else:
             raise NotImplementedError(mode)
+
         return prompt
 
-    def generate(url, conf, text):
-        headers = {"Content-Type": "application/json"}
-        c = conf.copy()
-        c["prompt"] = text
-        response = requests.post(url + "/generate", json=c, stream=True, verify=False)
-        res = b""
-        for r in response:
-            res += r
-        ans = json.loads(res.decode("utf-8"))
-        ans = ans["text"][0]
-        return ans
+    def has_data(self):
+        return os.path.exists(self.outdir)
 
     def run(self):
         # Input validation
@@ -64,7 +138,7 @@ class EVAL(object):
             print("Model unknwon: %s" % self.model)
             exit()
 
-        if os.path.exists(self.outdir):
+        if self.has_data():
             if not self.yes:
                 user_input = input(
                     f"Path {self.outdir} already exists. Evaluation data will be overwritten.\nDo you want to continue? (Y/n): "
@@ -120,7 +194,7 @@ class EVAL(object):
 
             rows.append(
                 {
-                    "id":expid,
+                    "id": expid,
                     "words": data_x["words"],
                     "ttr": data_x["ttr"],
                     "artefacts": len(data_x["artefacts"]),
@@ -131,24 +205,24 @@ class EVAL(object):
             )
 
         df = pd.DataFrame(rows)
-        df.to_csv(self.outdir + 'res.csv', index=False)
+        df.to_csv(self.outdir + "res.csv", index=False)
 
 
 # async
 def eval_one(args: dict):
     model = args["model"]
     settings_vllm = args["settings_vllm"]
-    route = EVAL.ROUTES[model]
-    url = route["url"]
     doc = args["exp"]
     outdir = args["outdir"]
+    route = EVAL.ROUTES[model]
+    url = route["url"]
 
     # Make prompt
     make_prompt = getattr(EVAL, route["prompt_maker"])
     prompt = make_prompt(doc)
 
     # Generate answer
-    answer = EVAL.generate(url, settings_vllm, prompt)
+    answer = generate(url, settings_vllm, prompt)
 
     # Save answer
     with open(f'{outdir}/{doc["id_experience"]}.txt', "w", encoding="utf-8") as f:
@@ -165,8 +239,10 @@ def evaluate(
 
     eva = EVAL(model, version, limit, yes)
 
+    if not eva.has_data() or not to_:
+        # Re-run if no data or if --csv is not passed (overwrite)
+        np.random.seed(2) # @warning: this does not control the LLM seed (remote API)
+        eva.run()
+
     if to_:
         eva.to_csv()
-    else:
-        np.random.seed(2)
-        eva.run()
---
+++ evaluation/utils.py
@@ -0,0 +1,27 @@
+import json
+
+import requests
+
+
+def get_embedding_e5(text: str) -> list:
+    """OpenAI-like embedding API"""
+    host = "localhost:8080"
+    url = f"https://{host}/api/embedding"
+    headers = {"Content-Type": "application/json"}
+    query = {"text": text}
+    res = requests.post(url, headers=headers, data=json.dumps(query), verify=False).json()
+    return res
+
+
+def generate(url, conf, text):
+    """OpenAI-like completion API"""
+    headers = {"Content-Type": "application/json"}
+    c = conf.copy()
+    c["prompt"] = text
+    response = requests.post(url + "/generate", json=c, stream=True, verify=False)
+    res = b""
+    for r in response:
+        res += r
+    ans = json.loads(res.decode("utf-8"))
+    ans = ans["text"][0]
+    return ans
