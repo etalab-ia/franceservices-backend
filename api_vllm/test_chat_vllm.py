bae5492ea9bf41cd6323fadd9750662cab251f2d
dtrckd <dtrckd@gmail.com>
2023-11-16 02:43:06 +0100

    fix: legacy URL.

--- api_vllm/test_chat_vllm.py
+++ api_vllm/test_chat_vllm.py
@@ -20,7 +20,7 @@ while 1:
     #print(dialog)
 
     # Send POST request with string parameter
-    url = "http://localhost:8081"
+    url = "http://localhost:8082"
     headers = {"Content-Type": "application/json"}
     data = {
         "prompt": dialog,
