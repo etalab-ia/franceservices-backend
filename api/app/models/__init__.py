a6bace0198db8a09d02b8d69cccc59ad4a2e68b7
pedevineau <pierre-etienne.devineau@mines-paristech.fr>
2024-02-01 14:27:23 +0000

    hotfix: put back __init__.py (disappeared because of bad linting)

---
+++ api/app/models/__init__.py
@@ -0,0 +1,5 @@
+from .chat import Chat  # noqa
+from .login import BlacklistToken, PasswordResetToken  # noqa
+from .stream import Stream, SourceEnum  # noqa
+from .feedback import Feedback  # noqa
+from .user import User  # noqa
