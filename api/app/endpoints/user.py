913169f87a9fa8a7162ce2cba20754bbcc4fac6b
Adrien Carpentier <me@adriencarpentier.com>
2024-02-16 02:07:04 +0900

    chore: remove useless import

--- api/app/endpoints/user.py
+++ api/app/endpoints/user.py
@@ -1,9 +1,11 @@
-from app import crud, models, schemas
-from app.clients.mailjet_client import MailjetClient
-from app.config import FIRST_ADMIN_EMAIL, CONTACT_EMAIL
-from app.deps import get_current_user, get_db
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+
+from app import crud, models, schemas
+from app.clients.mailjet_client import MailjetClient
+from app.config import CONTACT_EMAIL
+from app.deps import get_current_user, get_db
+
 
 router = APIRouter()
 
