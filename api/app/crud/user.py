a48657e3bfb0d68762cc6600b9870dc567917719
dtrckd <dtrckd@gmail.com>
2024-03-06 16:43:29 +0100

    hotfix: fic password encoding

--- api/app/crud/user.py
+++ api/app/crud/user.py
@@ -5,17 +5,18 @@ from sqlalchemy.orm import Session
 from app import models, schemas
 
 
-def get_hashed_password(password):
+def get_hashed_password(password:str) -> str:
     # Use solely bcrypt instead of passlib, due do this issue: see https://github.com/pyca/bcrypt/issues/684#issuecomment-1902590553
     pwd_bytes = password.encode("utf-8")
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
-    return hashed_password
+    return hashed_password.decode("utf-8")
 
 
-def verify_password(plain_password, hashed_password):
-    password_byte_enc = plain_password.encode("utf-8")
-    return bcrypt.checkpw(password=password_byte_enc, hashed_password=hashed_password)
+def verify_password(plain_password:str, hashed_password:str) -> bool:
+    password_byte_bytes = plain_password.encode("utf-8")
+    hashed_password_bytes = hashed_password.encode("utf-8")
+    return bcrypt.checkpw(password=password_byte_bytes, hashed_password=hashed_password_bytes)
 
 
 def get_pending_users(db: Session):
