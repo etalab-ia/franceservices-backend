e4bd9e50576d1ee2b0d4af4c51edbf415fcb9794
dtrckd <dtrckd@gmail.com>
2024-05-30 19:02:28 +0200

    dummy: pipeline job locked

--- pyalbert/albert.py
+++ pyalbert/albert.py
@@ -56,13 +56,13 @@ if __name__ == "__main__":
     # Parse CLI arguments
     args = docopt(__doc__, version="0")
     debug = True if args["--debug"] else False
-    
+
     if args["create_whitelist"]:
         from pyalbert.whitelist import create_whitelist, download_directory
 
         # if --storage-dir is not provided, use default path /data/whitelist
         storage_dir = "/data/whitelist" if args["--storage-dir"] is None else args["--storage-dir"]
-      
+
         # @DEBUG: this modularity has side effect since the whiltelist path is hardcoded
         #         in the prompt.postprocessing module that uses this file...
         config_file = (
@@ -117,3 +117,5 @@ if __name__ == "__main__":
                 )
     else:
         raise NotImplementedError
+
+# Please install gitea
